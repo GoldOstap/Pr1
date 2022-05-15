8bfe090b9ad87160efacf4c46995946f57b2f87b
GoldOstap <izzheurov@mail.ru>
2022-05-15 21:19:57 +0400

    1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    int a;
+    String s;
+    Scanner scan= new Scanner(System.in);
+    System.out.print("Введите a ");
+    a = scan.nextInt();
+
+
+    s = (a>5 ? "Больше пяти" : "меньше пяти");
+
+
+
+
+    System.out.println(s);
+        //123
+    }
+
+}
